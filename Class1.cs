9128fe2dc5f2cc7cb5207a087e6b75d61b7e92d1
vysh1234 <vyshnavivinjam@gmail.com>
2021-10-26 15:37:18 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mymath
+{
+    public static class Class1
+    {
+
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
